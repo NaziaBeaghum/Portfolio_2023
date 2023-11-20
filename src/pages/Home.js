c374adce343297c26b3ca8a1c056f60637347b33
NaziaBeaghum <nazia.mk2@gmail.com>
2023-11-20 13:33:03 +0530

    first commit

---
+++ src/pages/Home.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import './pages.css'
+import Content from '../Components/Content'
+import Projects from './Projects'
+import Technologies from './Technologies'
+
+
+const Home = () => {
+  return (
+    <div>
+        
+       <Content/>
+       <Projects/>
+       <Technologies/>
+       
+    </div>
+  )
+}
+
+export default Home
